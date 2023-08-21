03dcda2e437cda85fb36f60708015bf700fc6772
TripleFlacko <anton.anton0v@icloud.com>
2023-08-21 17:22:54 +0300

    Added more exercises

---
+++ Programming Fundamentals with C#/4.2 Methods - More Exercise/04. Tribonacci Sequence.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int num = int.Parse(Console.ReadLine());
+
+        int[] tribonacciSequence = GenerateTribonacci(num);
+
+        Console.WriteLine(string.Join(" ", tribonacciSequence));
+    }
+
+    static int[] GenerateTribonacci(int num)
+    {
+        int[] tribonacciSequence = new int[num];
+        if (num >= 1)
+            tribonacciSequence[0] = 1;
+        if (num >= 2)
+            tribonacciSequence[1] = 1;
+        if (num >= 3)
+            tribonacciSequence[2] = 2;
+
+        for (int i = 3; i < num; i++)
+        {
+            tribonacciSequence[i] = tribonacciSequence[i - 1] + tribonacciSequence[i - 2] + tribonacciSequence[i - 3];
+        }
+
+        return tribonacciSequence;
+    }
+}
