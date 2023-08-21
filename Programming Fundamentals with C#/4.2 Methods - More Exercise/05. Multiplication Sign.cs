1e4c4275d11e25a7dc7441e27475fb2cc659cddd
TripleFlacko <anton.anton0v@icloud.com>
2023-08-21 17:24:27 +0300

    Added more exercises

---
+++ Programming Fundamentals with C#/4.2 Methods - More Exercise/05. Multiplication Sign.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int num1 = int.Parse(Console.ReadLine());
+        int num2 = int.Parse(Console.ReadLine());
+        int num3 = int.Parse(Console.ReadLine());
+
+        if (num1 == 0 || num2 == 0 || num3 == 0)
+        {
+            Console.WriteLine("zero");
+            return;
+        }
+
+        bool isNegative = true;
+        isNegative = isNegativeNum(num1, isNegative);
+        isNegative = isNegativeNum(num2, isNegative);
+        isNegative = isNegativeNum(num3, isNegative);
+        Console.WriteLine(isNegative ? "positive" : "negative");
+    }
+
+    static bool isNegativeNum(int num, bool isNegative)
+    {
+        if (num < 0)
+        {
+            isNegative = isNegative ? false : true;
+        }
+        return isNegative;
+    }
+}
