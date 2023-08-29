ad863b716423528cc8b32a2cfe3670a51f68719c
TripleFlacko <anton.anton0v@icloud.com>
2023-08-29 17:06:40 +0300

    Added more exercises

---
+++ Programming Fundamentals with C#/5.2 Lists - More Exercise/04. Mixed up Lists.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var first = Console.ReadLine().Split().Select(int.Parse).ToList();
+        var second = Console.ReadLine().Split().Select(int.Parse).ToList();
+        var finalList = new List<int>();
+        finalList.AddRange(first);
+        finalList.AddRange(second);
+
+        List<int> constraints = ExctractConstraints(first, second);
+
+        finalList = finalList.Where(x => x > constraints[0] && x < constraints[1]).ToList();
+        finalList.Sort();
+        Console.WriteLine(string.Join(" ", finalList));
+    }
+
+    static List<int> ExctractConstraints(List<int> first, List<int> second)
+    {
+        var limiter = first.Count < second.Count ? first.Count : second.Count;
+
+        for (int i = 0; i < limiter; i++)
+        {
+            first.RemoveAt(0);
+        }
+        for (int i = limiter; i > 0; i--)
+        {
+            second.RemoveAt(second.Count - 1);
+        }
+
+        var constraints = first.Count > second.Count ? first : second;
+        constraints.Sort();
+        return constraints;
+    }
+}
---
+++ Programming Fundamentals with C#/5.2 Lists - More Exercise/05. Drum Set.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var savings = double.Parse(Console.ReadLine());
+        var initialQuality = Console.ReadLine().Split().Select(int.Parse).ToList();
+        var currentQuality = new List<int>(initialQuality);
+
+        Calculations(ref savings, initialQuality, currentQuality);
+
+        Console.WriteLine(string.Join(' ', currentQuality));
+        Console.WriteLine($"Gabsy has {savings:F2}lv.");
+    }
+
+    static void Calculations(ref double savings, List<int> initialQuality, List<int> currentQuality)
+    {
+        string command;
+        while ((command = Console.ReadLine()) != "Hit it again, Gabsy!")
+        {
+            for (int i = 0; i < initialQuality.Count; i++)
+            {
+                currentQuality[i] -= int.Parse(command);
+
+                if (currentQuality[i] <= 0)
+                {
+                    if (savings - initialQuality[i] * 3 >= 0)
+                    {
+                        savings -= initialQuality[i] * 3;
+                        currentQuality[i] = initialQuality[i];
+                    }
+                    else
+                    {
+                        initialQuality.RemoveAt(i);
+                        currentQuality.RemoveAt(i);
+                        i--;
+                    }
+                }
+            }
+        }
+    }
+}
