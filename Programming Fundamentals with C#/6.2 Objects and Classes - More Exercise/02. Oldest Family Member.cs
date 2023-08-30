b39e9c4220b034c42853d9fe907c600a5c29a1bc
TripleFlacko <anton.anton0v@icloud.com>
2023-08-30 12:18:35 +0300

    Added more exercises

---
+++ Programming Fundamentals with C#/6.2 Objects and Classes - More Exercise/02. Oldest Family Member.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int number = int.Parse(Console.ReadLine());
+        var family = new Family();
+
+        for (int i = 0; i < number; i++)
+        {
+            var input = Console.ReadLine().Split().ToList();
+            string name = input[0];
+            int age = int.Parse(input[1]);
+            var person = new Person(name, age);
+            family.AddMember(person);
+        }
+        Console.WriteLine(family.GetOldestMember().Name + " " + family.GetOldestMember().Age);
+    }
+}
+class Family
+{
+    public Family()
+    {
+        People = new List<Person>();
+    }
+    public List<Person> People { get; set; }
+    public void AddMember(Person person)
+    {
+        People.Add(person);
+    }
+    public Person GetOldestMember()
+    {
+        Person oldestMember = People
+            .OrderByDescending(x => x.Age)
+            .First();
+
+        return oldestMember;
+    }
+}
+class Person
+{
+    public Person(string name, int age)
+    {
+        Name = name;
+        Age = age;
+    }
+    public int Age { get; set; }
+    public string Name { get; set; }
+}
---
+++ Programming Fundamentals with C#/6.2 Objects and Classes - More Exercise/03. Speed Racing.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Xml.Schema;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int lines = int.Parse(Console.ReadLine());
+        List<Car> cars = new List<Car>();
+
+        string command;
+        while ((command = Console.ReadLine()) != "End")
+        {
+            var carInfo = command.Split().ToArray();
+
+            if (carInfo[0] == "Drive")
+            {
+                cars
+                    .Where(x => x.Model == carInfo[1])
+                    .First()
+                    .Drive(double.Parse(carInfo[2]));
+            }
+            else
+            {
+                string model = carInfo[0];
+                double fuel = double.Parse(carInfo[1]);
+                double consumption = double.Parse(carInfo[2]);
+                var car = new Car(model, fuel, consumption);
+                cars.Add(car);
+            }
+
+        }
+
+        foreach (var car in cars)
+        {
+            Console.WriteLine($"{car.Model} {car.Fuel:F2} {car.Distance}");
+        }
+    }
+}
+class Car
+{
+    public Car(string model, double fuel, double consumption)
+    {
+        Model = model;
+        Fuel = fuel;
+        Consumption = consumption;
+        Distance = 0d;
+    }
+    public string Model { get; set; }
+    public double Fuel { get; set; }
+    public double Consumption { get; set; }
+    public double Distance { get; set; }
+
+    public void Drive(double distance)
+    {
+        if (Fuel >= distance * Consumption)
+        {
+            Fuel -= distance * Consumption;
+            Distance += distance;
+        }
+        else
+        {
+            Console.WriteLine("Insufficient fuel for the drive");
+        }
+    }
+}
