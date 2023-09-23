a8ace5ed5c95e5fd28e72d8db53fe524e2b1c19f
Anton Antonov <anton.anton0v@icloud.com>
2023-09-24 02:01:17 +0300

    Added more exercises

---
+++ 3. C# Advanced/1. Stacks and Queues/1. Reverse a String.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        string input = Console.ReadLine();
+
+        Stack<char> stack = new Stack<char>(input);
+        int number = stack.Count;
+
+        for (int i = 0; i < number; i++)
+        {
+            Console.Write(stack.Pop());
+        }
+    }
+}
---
+++ 3. C# Advanced/1. Stacks and Queues/2. Stack Sum.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        var input = Console.ReadLine().Split().Select(int.Parse);
+        Stack<int> stack = new Stack<int>(input);
+
+        string command;
+        while ((command = Console.ReadLine()).ToLower() != "end")
+        {
+            var commands = command.Split();
+            switch (commands[0].ToLower())
+            {
+                case "add":
+                    stack.Push(int.Parse(commands[1]));
+                    stack.Push(int.Parse(commands[2]));
+                    break;
+                case "remove":
+                    int number = int.Parse(commands[1]);
+                    if (number > stack.Count)
+                    {
+                        break;
+                    }
+                    for (int i = 0; i < number; i++)
+                    {
+                        stack.Pop();
+                    }
+                    break;
+                default:
+                    break;
+            }
+        }
+        Console.WriteLine($"Sum: {stack.ToArray().Sum()}");
+    }
+}
