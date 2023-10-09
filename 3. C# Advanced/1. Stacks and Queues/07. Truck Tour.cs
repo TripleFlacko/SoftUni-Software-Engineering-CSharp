7aa763f4efa4668f41fbafcdad6c158ae320b9fe
Anton Antonov <anton.anton0v@icloud.com>
2023-10-09 18:40:04 +0300

    Create 07. Truck Tour.cs

---
+++ 3. C# Advanced/1. Stacks and Queues/07. Truck Tour.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var gasStations = int.Parse(Console.ReadLine());
+        var pumps = new Queue<string>();
+
+        for (int i = 0; i < gasStations; i++)
+        {
+            pumps.Enqueue(Console.ReadLine());
+        }
+        for (int i = 0; i < gasStations; i++)
+        {
+            if (CanCompleteCircle(pumps))
+            {
+                Console.WriteLine(i);
+                return;
+            }
+            pumps.Enqueue(pumps.Dequeue());
+        }
+    }
+
+    private static bool CanCompleteCircle(Queue<string> pumps)
+    {
+        var fuel = 0;
+        bool canCompleteCircle = true;
+
+        for (int i = 0; i < pumps.Count; i++)
+        {
+            var gas = pumps.Peek().Split().Select(int.Parse).First();
+            var distance = pumps.Peek().Split().Select(int.Parse).Last();
+            fuel += gas - distance;
+
+            if (fuel < 0)
+            {
+                canCompleteCircle = false;
+            }
+            pumps.Enqueue(pumps.Dequeue());
+        }
+        return canCompleteCircle;
+    }
+}
