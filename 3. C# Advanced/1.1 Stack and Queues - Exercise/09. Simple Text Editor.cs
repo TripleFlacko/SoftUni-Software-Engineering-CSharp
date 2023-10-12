816ed94a3aaa320bf30d9b04f7682f645fbca24a
Anton Antonov <119543923+TripleFlacko@users.noreply.github.com>
2023-10-12 15:42:49 +0300

    Create 09. Simple Text Editor.cs

---
+++ 3. C# Advanced/1.1 Stack and Queues - Exercise/09. Simple Text Editor.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var states = new Stack<string>();
+        states.Push(string.Empty);
+        var numberOfCommands = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i < numberOfCommands; i++)
+        {
+            var input = Console.ReadLine().Split();
+            var command = input[0];
+            var value = input.Last();
+
+            switch (command)
+            {
+                case "1":
+                    states.Push(states.Peek() + value);
+                    break;
+
+                case "2":
+                    var lettersToRemove = int.Parse(value);
+                    var newState = states.Peek().Remove(states.Peek().Length - lettersToRemove);
+                    states.Push(newState);
+                    break;
+                case "3":
+                    var index = int.Parse(value);
+                    var foundChar = states.Peek()[index - 1];
+                    Console.WriteLine(foundChar);
+                    break;
+                case "4":
+                    states.Pop();
+                    break;
+            }
+        }
+    }
+}
