2124b4ddc46cae383ef1812ec6605ecf38706c47
Anton Antonov <119543923+TripleFlacko@users.noreply.github.com>
2023-10-12 15:42:39 +0300

    Create 08. Balanced Parenthesis.cs

---
+++ 3. C# Advanced/1.1 Stack and Queues - Exercise/08. Balanced Parenthesis.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var brackets = Console.ReadLine();
+        var stack = new Stack<char>();
+
+        foreach (var bracket in brackets)
+        {
+            if (IsOpeningBracket(bracket))
+            {
+                stack.Push(bracket);
+            }
+            else
+            {
+                if (stack.Count == 0 || !IsMatchingPair(stack.Pop(), bracket))
+                {
+                    Console.WriteLine("NO");
+                    return;
+                }
+            }
+        }
+        Console.WriteLine(stack.Count == 0 ? "YES" : "NO");
+    }
+
+    static bool IsMatchingPair(char openingBracket, char closingBracket)
+    {
+        return (openingBracket == '(' && closingBracket == ')') || (openingBracket == '[' && closingBracket == ']') || (openingBracket == '{' && closingBracket == '}');
+    }
+
+    static bool IsOpeningBracket(char bracket)
+    {
+        return bracket == '(' || bracket == '[' || bracket == '{';
+    }
+}
