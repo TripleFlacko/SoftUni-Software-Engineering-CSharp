784079b2e898a0d592bd0c79dc928162f4af393b
Anton Antonov <119543923+TripleFlacko@users.noreply.github.com>
2023-10-24 16:24:37 +0300

    Create 08. SoftUni Party.cs

---
+++ 3. C# Advanced/3. Sets and Dictionaries Advanced - Lab/08. SoftUni Party.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Numerics;
+
+class Program
+{
+    static void Main()
+    {
+        var guestList = new HashSet<string>();
+
+        while (true)
+        {
+            var input = Console.ReadLine();
+
+            if (input == "PARTY")
+            {
+                while (true)
+                {
+                    var guestArrived = Console.ReadLine();
+                    guestList.Remove(guestArrived);
+                    if (guestArrived == "END")
+                    {
+                        guestList = guestList.OrderByDescending(x => char.IsDigit(x[0])).ToHashSet();
+                        break;
+                    }
+                }
+                break;
+            }
+            else
+            {
+                guestList.Add(input);
+            }
+        }
+        Console.WriteLine($"{guestList.Count}\n" +
+            $"{string.Join("\n", guestList)}");
+    }
+}
