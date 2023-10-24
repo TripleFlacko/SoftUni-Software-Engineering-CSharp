13d8a12d87394ac0ed5e84ffc76f0b54dff65122
Anton Antonov <119543923+TripleFlacko@users.noreply.github.com>
2023-10-24 09:47:51 +0300

    Added more exercises

---
+++ 3. C# Advanced/3. Sets and Dictionaries Advanced - Lab/03. Largest 3 Numbers.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Numerics;
+
+class Program
+{
+    static void Main()
+    {
+        var input = Console.ReadLine().Split().Select(int.Parse).OrderByDescending(x => x).ToArray();
+        for (int i = 0; i < 3; i++)
+        {
+            if (i == input.Length)
+            {
+                break;
+            }
+            Console.Write(input[i] + " ");
+        }
+    }
+}
---
+++ 3. C# Advanced/3. Sets and Dictionaries Advanced - Lab/04. Product Shop.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Numerics;
+
+class Program
+{
+    static void Main()
+    {
+        var markets = new Dictionary<string, Dictionary<string, double>>();
+        string input;
+        while ((input = Console.ReadLine()) != "Revision")
+        {
+            var data = input.Split(", ", StringSplitOptions.RemoveEmptyEntries).ToArray();
+            if (markets.ContainsKey(data[0]))
+            {
+                markets[data[0]].Add(data[1], double.Parse(data[2]));
+            }
+            else
+            {
+                markets[data[0]] = new Dictionary<string, double>() { { data[1], double.Parse(data[2]) } };
+            }
+        }
+        foreach (var market in markets.OrderBy(s => s.Key))
+        {
+            Console.WriteLine($"{market.Key}->");
+            Console.WriteLine(string.Join("\n", market.Value.Select(x => $"Product: {x.Key}, Price: {x.Value}")));
+        }
+    }
+}
