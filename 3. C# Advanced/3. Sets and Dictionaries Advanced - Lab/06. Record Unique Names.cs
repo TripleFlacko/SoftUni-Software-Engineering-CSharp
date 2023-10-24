22727e646b147d1480fbf00a852caba024d66d96
Anton Antonov <119543923+TripleFlacko@users.noreply.github.com>
2023-10-24 15:51:03 +0300

    Create 06. Record Unique Names.cs

---
+++ 3. C# Advanced/3. Sets and Dictionaries Advanced - Lab/06. Record Unique Names.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Numerics;
+
+class Program
+{
+    static void Main()
+    {
+        var parking = new HashSet<string>();
+
+        string input;
+        while ((input = Console.ReadLine()) != "END")
+        {
+            var data = input.Split(", ");
+            var direction = data[0];
+            var licensePlate = data[1];
+
+            switch (direction)
+            {
+                case "IN":
+                    parking.Add(licensePlate);
+                    break;
+                case "OUT":
+                    parking.Remove(licensePlate);
+                    break;
+            }
+        }
+        Console.WriteLine(parking.Count > 0 ? string.Join("\n", parking) : "Parking Lot is Empty");
+    }
+}
