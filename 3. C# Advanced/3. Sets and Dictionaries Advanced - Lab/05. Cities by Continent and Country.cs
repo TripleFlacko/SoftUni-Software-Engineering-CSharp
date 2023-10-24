57792d38b788593a97c623604c2655dd9d2272ab
Anton Antonov <119543923+TripleFlacko@users.noreply.github.com>
2023-10-24 15:14:21 +0300

    Create 05. Cities by Continent and Country.cs

---
+++ 3. C# Advanced/3. Sets and Dictionaries Advanced - Lab/05. Cities by Continent and Country.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Numerics;
+
+class Program
+{
+    static void Main()
+    {
+        var database = new Dictionary<string, Dictionary<string, List<string>>>();
+        var lines = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i < lines; i++)
+        {
+            var input = Console.ReadLine().Split();
+            var continent = input[0];
+            var country = input[1];
+            var city = input[2];
+
+            if (database.ContainsKey(continent))
+            {
+                if (database[continent].ContainsKey(country))
+                {
+                    database[continent][country].Add(city);
+                }
+                else
+                {
+                    database[continent][country] = new List<string>() { city };
+                }
+            }
+            else
+            {
+                database[continent] = new Dictionary<string, List<string>>()
+                {
+                    { country, new List<string>(){ city } }
+                };
+            }
+        }
+        foreach (var continent in database)
+        {
+            Console.WriteLine($"{continent.Key}:");
+            foreach (var country in continent.Value)
+            {
+                Console.WriteLine($"{country.Key} -> {string.Join(", ", country.Value)}");
+            }
+        }
+    }
+}
