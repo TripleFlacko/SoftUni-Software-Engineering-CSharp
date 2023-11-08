7b18b4633e40c9a8d0e6236a6971322655f442af
Anton Antonov <119543923+TripleFlacko@users.noreply.github.com>
2023-11-08 12:01:40 +0200

    Added more exercises

---
+++ 3. C# Advanced/6.1.Defining-Classes-Lab/03.CarConstructors/StartUp.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CarManufacturer;
+
+namespace CarManufacturer
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var car = new Car();
+
+            car.Make = "VW";
+            car.Model = "MK3";
+            car.Year = 1992;
+            car.FuelQuantity = 200;
+            car.FuelConsumption = 200;
+            car.Drive(2000);
+            Console.WriteLine(car.WhoAmI());
+        }
+    }
+}
