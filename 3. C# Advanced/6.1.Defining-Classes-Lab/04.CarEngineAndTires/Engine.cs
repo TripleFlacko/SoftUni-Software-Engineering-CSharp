0b2e01e554eb2c94355d8e14e3902b37524d57fd
Anton Antonov <119543923+TripleFlacko@users.noreply.github.com>
2023-11-08 17:12:37 +0200

    Added more exercises

---
+++ 3. C# Advanced/6.1.Defining-Classes-Lab/04.CarEngineAndTires/Engine.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CarManufacturer;
+
+
+namespace CarManufacturer
+{
+    public class Engine
+    {
+        public Engine(int horsePower, double cubicCapacity)
+        {
+			HorsePower = horsePower;
+			CubicCapacity = cubicCapacity;
+        }
+        private int horsePower;
+
+		private double cubicCapacity;
+
+		public double CubicCapacity
+		{
+			get { return cubicCapacity; }
+			set { cubicCapacity = value; }
+		}
+
+		public int HorsePower
+		{
+			get { return horsePower; }
+			set { horsePower = value; }
+		}
+	}
+}
---
+++ 3. C# Advanced/6.1.Defining-Classes-Lab/04.CarEngineAndTires/StartUp.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CarManufacturer;
+
+namespace CarManufacturer
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var tires = new Tire[4]
+            {
+                new Tire(1, 2.5),
+                new Tire(1, 2.1),
+                new Tire(2, 0.5),
+                new Tire(2, 2.3)
+            };
+
+            var engine = new Engine(560, 6300);
+
+            var car = new Car("Lamborghini", "Urus", 2010, 250, 9, engine, tires);
+        }
+    }
+}
