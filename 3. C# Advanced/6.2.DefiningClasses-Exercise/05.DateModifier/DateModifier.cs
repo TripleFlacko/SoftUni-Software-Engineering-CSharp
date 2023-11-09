5332d9240af228086b2f5085febf4bbe8690d57e
Anton Antonov <119543923+TripleFlacko@users.noreply.github.com>
2023-11-09 11:51:58 +0200

    Added more exercises

---
+++ 3. C# Advanced/6.2.DefiningClasses-Exercise/05.DateModifier/DateModifier.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DateModifier
+{
+    public class DateModifier
+    {
+		public static double GetDifference(DateTime startDate, DateTime endDate)
+		{
+			var difference = Math.Abs((startDate - endDate).TotalDays);
+            return difference;
+		}
+	}
+}
---
+++ 3. C# Advanced/6.2.DefiningClasses-Exercise/05.DateModifier/StartUp.cs
@@ -0,0 +1,13 @@
+﻿using DateModifier;
+namespace DateModifier
+{
+    internal class StartUp
+    {
+        static void Main()
+        {
+            var startDate = DateTime.Parse(Console.ReadLine());
+            var endDate = DateTime.Parse(Console.ReadLine());
+            Console.WriteLine(DateModifier.GetDifference(startDate, endDate));
+        }
+    }
+}
