626efac47952f90cb3b2ace16e7b4a7239e1ed96
Anton Antonov <119543923+TripleFlacko@users.noreply.github.com>
2023-11-09 09:24:50 +0200

    Added more exercises

--- 3. C# Advanced/6.2.DefiningClasses-Exercise/02.CreatingConstructors/Person.cs
+++ 3. C# Advanced/6.2.DefiningClasses-Exercise/02.CreatingConstructors/Person.cs
@@ -10,7 +10,22 @@ namespace DefiningClasses
     public class Person
     {
 		private string name;
+
 		private int age;
+        public Person()
+        {
+			Name = "No name";
+			Age = 1;
+        }
+        public Person(int age) : this()
+        {
+			Age = age;
+        }
+        public Person(string name, int age) : this(age)
+        {
+            Name = name;
+			Age = age;
+        }
         public string Name
 		{
 			get { return name; }
