e3633ef65f405aec26cf77f5f2b658c08700f266
Anton Antonov <anton.anton0v@icloud.com>
2023-11-11 15:43:03 +0200

    Added more exercises

---
+++ 3.C#-Advanced/6.2.DefiningClasses-Exercise/10.SoftUniParking/Car.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoftUniParking
+{
+    public class Car
+    {
+        private string make;
+        private string model;
+        private int horsePower;
+        private string registrationNumber;
+        public Car(string make, string model, int horsePower, string registrationNumber)
+        {
+            Make = make;
+            Model = model;
+            HorsePower = horsePower;
+            RegistrationNumber = registrationNumber;
+        }
+        public string RegistrationNumber
+        {
+            get { return registrationNumber; }
+            set { registrationNumber = value; }
+        }
+
+        public int HorsePower
+        {
+            get { return horsePower; }
+            set { horsePower = value; }
+        }
+
+        public string Model
+        {
+            get { return model; }
+            set { model = value; }
+        }
+
+        public string Make
+        {
+            get { return make; }
+            set { make = value; }
+        }
+
+        public override string ToString()
+        {
+            return $"Make: {Make}\n" +
+                $"Model: {Model}\n" +
+                $"HorsePower: {HorsePower}\n" +
+                $"RegistrationNumber: {RegistrationNumber}";
+        }
+    }
+}
---
+++ 3.C#-Advanced/6.2.DefiningClasses-Exercise/10.SoftUniParking/Parking.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Metrics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoftUniParking
+{
+    public class Parking
+    {
+        private Dictionary<string, Car> cars;
+        private int capacity;
+        private int count;
+        public Parking(int capacity)
+        {
+            cars = new Dictionary<string, Car>();
+            Capacity = capacity;
+        }
+        public int Count
+        {
+            get { return Cars.Count; }
+            set { count = Cars.Count; }
+        }
+
+        public int Capacity
+        {
+            get { return capacity; }
+            set { capacity = value; }
+        }
+
+        public Dictionary<string, Car> Cars
+        {
+            get { return cars; }
+            set { cars = value; }
+        }
+
+        public string AddCar(Car car)
+        {
+            if (Cars.ContainsKey(car.RegistrationNumber))
+            {
+                return "Car with that registration number, already exists!";
+            }
+            if (cars.Count < Capacity)
+            {
+                cars[car.RegistrationNumber] = car;
+                return $"Successfully added new car {car.Make} {car.RegistrationNumber}";
+            }
+            else
+            {
+                return "Parking is full!";
+            }
+        }
+        public string RemoveCar(string registrationNumber)
+        {
+            if (Cars.ContainsKey(registrationNumber))
+            {
+                Cars.Remove(registrationNumber);
+                return $"Successfully removed {registrationNumber}";
+            }
+            else
+            {
+                return "Car with that registration number, doesn't exist!";
+            }
+        }
+        public Car GetCar(string registrationNumber)
+        {
+            return Cars.First(car=>car.Value.RegistrationNumber == registrationNumber).Value;
+        }
+        public void RemoveSetOfRegistrationNumber(List<string> RegistrationNumbers)
+        {
+            foreach (string registrationNumber in RegistrationNumbers)
+            {
+                if (Cars.ContainsKey(registrationNumber))
+                {
+                    Cars.Remove(registrationNumber);
+                }
+            }
+        }
+    }
+}
