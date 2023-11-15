ef8e22614aa6a6158e7053f2fc3b1daeba3fedb7
Anton Antonov <119543923+TripleFlacko@users.noreply.github.com>
2023-11-15 10:49:31 +0200

    Added more exercises

---
+++ 3.C#-Advanced/8.1.Generics-Lab/8.1.Generics-Lab/Box.cs
@@ -0,0 +1,26 @@
+﻿namespace BoxOfT
+{
+    public class Box<T>
+    {
+        public Stack<T> stack;
+        public int Count
+        {
+            get
+            {
+                return stack.Count;
+            }
+        }
+        public Box()
+        {
+            stack = new Stack<T>();
+        }
+        public void Add(T element)
+        {
+            stack.Push(element);
+        }
+        public T Remove()
+        {
+            return stack.Pop();
+        }
+    }
+}
