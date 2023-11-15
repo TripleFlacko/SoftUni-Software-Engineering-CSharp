2aaca37c9475e80c821bb3ead1029e37a997c89f
Anton Antonov <119543923+TripleFlacko@users.noreply.github.com>
2023-11-15 11:46:57 +0200

    Added more exercises

---
+++ 3.C#-Advanced/8.1.Generics-Lab/GenericScale/EqualityScale.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericScale
+{
+    public class EqualityScale<T> where T : struct,
+          IComparable,
+          IComparable<T>,
+          IConvertible,
+          IEquatable<T>,
+          IFormattable
+    {
+        public T Left { get; set; }
+        public T Right { get; set; }
+        public EqualityScale(T left, T right)
+        {
+            Left = left;
+            Right = right;
+        }
+        public bool AreEqual()
+        {
+            if (Left.Equals(Right))
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
