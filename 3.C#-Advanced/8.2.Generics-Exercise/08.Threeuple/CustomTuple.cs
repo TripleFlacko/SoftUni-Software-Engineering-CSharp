ba69a6aa454254204e0026e14edaae70d1e59771
Anton Antonov <119543923+TripleFlacko@users.noreply.github.com>
2023-11-16 17:00:00 +0200

    Added more exercises

---
+++ 3.C#-Advanced/8.2.Generics-Exercise/08.Threeuple/CustomTuple.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _07.Tuple
+{
+    public class CustomTuple<T, T2, T3>
+    {
+        public T Item1 { get; set; }
+        public T2 Item2 { get; set; }
+        public T3 Item3 { get; set; }
+
+        public void Create(T item1, T2 item2, T3 item3)
+        {
+            Item1 = item1;
+            Item2 = item2;
+            Item3 = item3;
+        }
+    }
+}
---
+++ 3.C#-Advanced/8.2.Generics-Exercise/08.Threeuple/Program.cs
@@ -0,0 +1,24 @@
+﻿using _07.Tuple;
+
+namespace _08.Threeuple
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var firstLine = Console.ReadLine().Split();
+            var firstTuple = new CustomTuple<string, string, string>();
+            firstTuple.Create(string.Join(' ', firstLine[..2]), firstLine[2], string.Join(' ', firstLine[3..]));
+
+            var secondLine = Console.ReadLine().Split();
+            var secondTuple = new CustomTuple<string, int, bool>();
+            secondTuple.Create(secondLine[0], int.Parse(secondLine[1]), secondLine[2] == "drunk" ? true : false);
+
+            var thirdLine = Console.ReadLine().Split();
+            var thirdTuple = new CustomTuple<string, double, string>();
+            thirdTuple.Create(thirdLine[0], double.Parse(thirdLine[1]), thirdLine[2]);
+
+            Console.WriteLine($"{firstTuple.Item1} -> {firstTuple.Item2} -> {firstTuple.Item3}\n{secondTuple.Item1} -> {secondTuple.Item2} -> {secondTuple.Item3}\n{thirdTuple.Item1} -> {thirdTuple.Item2} -> {thirdTuple.Item3}");
+        }
+    }
+}
