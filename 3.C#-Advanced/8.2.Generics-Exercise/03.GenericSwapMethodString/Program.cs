a9e897dc38b2325b704d99f8508c6c2c9dc1fd52
Anton Antonov <119543923+TripleFlacko@users.noreply.github.com>
2023-11-16 15:02:02 +0200

    Added more exercises

---
+++ 3.C#-Advanced/8.2.Generics-Exercise/03.GenericSwapMethodString/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace _03.GenericSwapMethodString
+{
+    public class Program
+    {
+        static void Main()
+        {
+            var box = new Box<string>();
+            var n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                box.AddValue(Console.ReadLine());
+            }
+            var swapIndexes = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            box.Swap(swapIndexes[0], swapIndexes[1]);
+            Console.WriteLine(box.ToString());
+        }
+    }
+}
