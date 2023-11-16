87f5e34920231a16f60708584bfa3cd88ead37aa
Anton Antonov <119543923+TripleFlacko@users.noreply.github.com>
2023-11-16 11:16:15 +0200

    Added more exercises

---
+++ 3.C#-Advanced/8.2.Generics-Exercise/01.GenericBoxOfString/Box.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01.GenericBoxOfString
+{
+    public class Box<T>
+    {
+        private readonly List<T> list = new List<T>();
+        public void AddValue(T value)
+        {
+            list.Add(value);
+        }
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+            foreach (var item in list)
+            {
+                sb.AppendLine($"{typeof(T)}: {item.ToString()}");
+            }
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
---
+++ 3.C#-Advanced/8.2.Generics-Exercise/01.GenericBoxOfString/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace _01.GenericBoxOfString
+{
+    public class Program
+    {
+        static void Main()
+        {
+            var box = new Box<string>();
+            var n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                box.AddValue(Console.ReadLine());
+            }
+            Console.WriteLine(box.ToString());
+        }
+    }
+}
