700f439da61f0bedbeede1a9a8826f4a8899e98d
Anton Antonov <119543923+TripleFlacko@users.noreply.github.com>
2023-11-16 15:04:22 +0200

    Added more exercises

---
+++ 3.C#-Advanced/8.2.Generics-Exercise/04.GenericSwapMethodInteger/Box.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04.GenericSwapMethodInteger
+{
+    public class Box<T>
+    {
+        private readonly List<T> list = new List<T>();
+
+        public void AddValue(T value)
+        {
+            list.Add(value);
+        }
+        public void Swap(int firstIndex, int secondIndex)
+        {
+            (list[firstIndex], list[secondIndex]) = (list[secondIndex], list[firstIndex]);
+        }
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+            foreach (var item in list)
+            {
+                sb.AppendLine($"{typeof(T)}: {item.ToString()}");
+            }
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
---
+++ 3.C#-Advanced/8.2.Generics-Exercise/04.GenericSwapMethodInteger/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace _04.GenericSwapMethodInteger
+{
+    public class Program
+    {
+        static void Main()
+        {
+            var box = new Box<int>();
+            var n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                box.AddValue(int.Parse(Console.ReadLine()));
+            }
+            var swapIndexes = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            box.Swap(swapIndexes[0], swapIndexes[1]);
+            Console.WriteLine(box.ToString());
+        }
+    }
+}
