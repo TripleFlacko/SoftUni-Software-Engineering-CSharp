a4c38178b5a67a3985812d6bf9cdca8814c3a37e
Anton Antonov <119543923+TripleFlacko@users.noreply.github.com>
2023-11-16 16:58:07 +0200

    Added more exercises

---
+++ 3.C#-Advanced/8.2.Generics-Exercise/07.Tuple/CustomTuple.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _07.Tuple
+{
+    public class CustomTuple<T, T2>
+    {
+        public T Item1 { get; set; }
+        public T2 Item2 { get; set; }
+
+        public void Create(T item1, T2 item2)
+        {
+            Item1 = item1;
+            Item2 = item2;
+        }
+    }
+}
---
+++ 3.C#-Advanced/8.2.Generics-Exercise/07.Tuple/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace _07.Tuple
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var firstLine = Console.ReadLine().Split();
+            var firstTuple = new CustomTuple<string, string>();
+            firstTuple.Create(string.Join(' ', firstLine[..2]), firstLine[2]);
+
+            var secondLine = Console.ReadLine().Split();
+            var secondTuple = new CustomTuple<string, int>();
+            secondTuple.Create(secondLine[0], int.Parse(secondLine[1]));
+
+            var thirdLine = Console.ReadLine().Split();
+            var thirdTuple = new CustomTuple<int, double>();
+            thirdTuple.Create(int.Parse(thirdLine[0]), double.Parse(thirdLine[1]));
+
+            Console.WriteLine($"{firstTuple.Item1} -> {firstTuple.Item2}\n{secondTuple.Item1} -> {secondTuple.Item2}\n{thirdTuple.Item1} -> {thirdTuple.Item2}");
+
+        }
+    }
+}
