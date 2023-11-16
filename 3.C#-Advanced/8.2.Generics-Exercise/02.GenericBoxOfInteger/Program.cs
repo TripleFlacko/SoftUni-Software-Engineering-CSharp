ed9235e14f9f2203cac8330bcc81040fefad21f0
Anton Antonov <119543923+TripleFlacko@users.noreply.github.com>
2023-11-16 13:49:53 +0200

    Added more exercises

---
+++ 3.C#-Advanced/8.2.Generics-Exercise/02.GenericBoxOfInteger/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace _02.GenericBoxOfInteger
+{
+    public class Program
+    {
+        static void Main()
+        {
+            var box = new Box<int>();
+            var n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                box.AddValue(int.Parse(Console.ReadLine()));
+            }
+            Console.WriteLine(box.ToString());
+        }
+    }
+}
