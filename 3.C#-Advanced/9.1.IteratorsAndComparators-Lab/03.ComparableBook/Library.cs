a949a032bb13a1924b2703be18bc129b666a412d
Anton Antonov <anton.anton0v@icloud.com>
2023-11-18 16:48:58 +0200

    Added more exercises

---
+++ 3.C#-Advanced/9.1.IteratorsAndComparators-Lab/03.ComparableBook/Library.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IteratorsAndComparators
+{
+    public class Library : IEnumerable<Book>
+    {
+        private SortedSet<Book> books;
+
+        public Library(params Book[] books)
+        {
+            Books = new SortedSet<Book>(books);
+        }
+        public SortedSet<Book> Books
+        {
+            get { return books; }
+            set { books = value; }
+        }
+
+        public IEnumerator<Book> GetEnumerator()
+        {
+            return new LibraryIterator(books);
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+
+        private class LibraryIterator : IEnumerator<Book>
+        {
+            private int index;
+            private readonly List<Book> books;
+
+            public LibraryIterator(IEnumerable<Book> books)
+            {
+                this.books = books.ToList();
+                Reset();
+            }
+
+            public Book Current => books[index];
+
+            object IEnumerator.Current => Current;
+
+            public void Dispose(){}
+
+            public bool MoveNext() => ++index < books.Count;
+
+            public void Reset() => index = -1;
+        }
+    }
+}
