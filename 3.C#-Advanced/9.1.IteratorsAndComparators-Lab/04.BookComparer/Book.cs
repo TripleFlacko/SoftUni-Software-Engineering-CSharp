96d858c18f3044c3ed90aa89ee29f88a60a55b2d
Anton Antonov <anton.anton0v@icloud.com>
2023-11-18 17:02:23 +0200

    Added more exercises

---
+++ 3.C#-Advanced/9.1.IteratorsAndComparators-Lab/04.BookComparer/Book.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IteratorsAndComparators
+{
+    public class Book : IComparable<Book>
+    {
+		private string title;
+		private int year;
+		private List<string> authors;
+
+        public Book(string title, int year, params string[] authors)
+        {
+			Title = title;
+			Year = year;
+            Authors = new List<string>(authors);
+        }
+
+        public List<string> Authors
+		{
+			get { return authors; }
+			set { authors = value; }
+		}
+
+		public int Year
+		{
+			get { return year; }
+			set { year = value; }
+		}
+
+
+		public string Title
+        {
+			get { return title; }
+			set { title = value; }
+		}
+
+        public int CompareTo(Book other)
+        {
+			int result = this.Year.CompareTo(other.Year);
+			if (result == 0)
+			{
+				return this.Title.CompareTo(other.Title);
+			}
+			return result;
+        }
+		public override string ToString()
+		{
+			return $"{Title} - {Year}";
+		}
+    }
+}
