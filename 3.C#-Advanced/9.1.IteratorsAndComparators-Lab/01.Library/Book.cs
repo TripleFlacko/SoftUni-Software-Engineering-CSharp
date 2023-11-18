85398047e0d77b9781e5683f02780355e3e90b29
Anton Antonov <anton.anton0v@icloud.com>
2023-11-18 16:30:53 +0200

    Added more exercises

---
+++ 3.C#-Advanced/9.1.IteratorsAndComparators-Lab/01.Library/Book.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IteratorsAndComparators
+{
+    public class Book
+    {
+		private string title;
+		private int year;
+		private IReadOnlyList<string> authors;
+
+        public Book(string title, int year, params string[] authors)
+        {
+			Title = title;
+			Year = year;
+            Authors = new List<string>(authors);
+        }
+
+        public IReadOnlyList<string> Authors
+		{
+			get { return authors; }
+			set { authors = value; }
+		}
+
+		public int Year
+		{
+			get { return year; }
+			set { year = value; }
+		}
+
+
+		public string Title
+        {
+			get { return title; }
+			set { title = value; }
+		}
+
+	}
+}
---
+++ 3.C#-Advanced/9.1.IteratorsAndComparators-Lab/01.Library/Library.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IteratorsAndComparators
+{
+    public class Library
+    {
+        private List<Book> books;
+
+        public Library(params Book[] books)
+        {
+            Books = new List<Book>(books);
+        }
+        public List<Book> Books
+        {
+            get { return books; }
+            set { books = value; }
+        }
+    }
+}
