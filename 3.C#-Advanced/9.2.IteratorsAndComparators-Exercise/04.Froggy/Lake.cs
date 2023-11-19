778848348614e9278f40ed95a7f8e22b193a95ae
Anton Antonov <anton.anton0v@icloud.com>
2023-11-19 12:04:07 +0200

    Added mroe exercises

---
+++ 3.C#-Advanced/9.2.IteratorsAndComparators-Exercise/04.Froggy/Lake.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04.Froggy
+{
+    public class Lake : IEnumerable<int>
+    {
+        public int[] _numbers;
+        public Lake(int[] numbers)
+        {
+            _numbers = numbers;
+        }
+
+        public IEnumerator<int> GetEnumerator()
+        {
+            for (int i = 0; i < _numbers.Length; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    yield return _numbers[i];
+                }
+            }
+            for (int i = _numbers.Length - 1; i >= 0; i--)
+            {
+                if (i % 2 != 0)
+                {
+                    yield return _numbers[i];
+                }
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+    }
+}
---
+++ 3.C#-Advanced/9.2.IteratorsAndComparators-Exercise/04.Froggy/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace _04.Froggy
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            var numbers = Console.ReadLine().Split(", ").Select(int.Parse).ToArray();
+            var lake = new Lake(numbers);
+            Console.WriteLine(string.Join(", ", lake));
+        }
+    }
+}
