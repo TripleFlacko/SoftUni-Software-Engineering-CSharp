729396abe5a9a38eb06792b3a002072582a28bbc
Anton Antonov <anton.anton0v@icloud.com>
2023-11-19 13:44:22 +0200

    Added more exercises

---
+++ 3.C#-Advanced/9.2.IteratorsAndComparators-Exercise/05.ComparingObjects/Person.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05.ComparingObjects
+{
+    public class Person : IComparable<Person>
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Town { get; set; }
+
+        public Person(string name, int age, string town)
+        {
+            Name = name;
+            Age = age;
+            Town = town;
+        }
+
+        public int CompareTo(Person? other)
+        {
+            var result = this.Name.CompareTo(other.Name);
+
+            if (result == 0)
+            {
+                result = this.Age.CompareTo(other.Age);
+            }
+
+            if (result == 0)
+            {
+                result = this.Town.CompareTo(other.Town);
+            }
+
+            return result;
+        }
+    }
+}
---
+++ 3.C#-Advanced/9.2.IteratorsAndComparators-Exercise/05.ComparingObjects/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace _05.ComparingObjects
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            var persons = new List<Person>();
+
+            string command;
+            while ((command = Console.ReadLine()) != "END")
+            {
+                var personInfo = command.Split(' ', StringSplitOptions.RemoveEmptyEntries).ToArray();
+                var name = personInfo[0];
+                var age = int.Parse(personInfo[1]);
+                var town = personInfo[2];
+
+                var person = new Person(name, age, town);
+                persons.Add(person);
+            }
+
+            var compareToPersonIndex = int.Parse(Console.ReadLine()) - 1;
+            var matching = 0;
+            var notMatching = 0;
+
+            foreach (var person in persons)
+            {
+                var result = person.CompareTo(persons[compareToPersonIndex]);
+                if (result == 0)
+                {
+                    matching++;
+                }
+                else
+                {
+                    notMatching++;
+                }
+            }
+
+            Console.WriteLine(matching == 1 ? "No matches" : $"{matching} {notMatching} {persons.Count}");
+        }
+    }
+}
