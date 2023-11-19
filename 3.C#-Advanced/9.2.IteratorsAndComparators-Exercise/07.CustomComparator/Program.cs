cac0143a4336c62a4fda51fa71267781fa5675b3
Anton Antonov <anton.anton0v@icloud.com>
2023-11-19 14:36:45 +0200

    Added more exercises

---
+++ 3.C#-Advanced/9.2.IteratorsAndComparators-Exercise/07.CustomComparator/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Immutable;
+
+namespace _07.CustomComparator
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            Array.Sort(numbers, new CustomComparator());
+            Console.WriteLine(string.Join(' ', numbers));
+        }
+    }
+    public class CustomComparator : IComparer<int>
+    {
+        public int Compare(int x, int y)
+        {
+            if (x % 2 == 0 && y % 2 != 0)
+            {
+                return -1;
+            }
+            if (y % 2 == 0 && x % 2 != 0)
+            {
+                return 1;
+            }
+            return x.CompareTo(y);
+        }
+    }
+}
