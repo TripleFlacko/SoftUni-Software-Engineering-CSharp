b6b5d98f0e364f90d80665ab9c4e2ccfbbd3cd94
Anton Antonov <anton.anton0v@icloud.com>
2023-11-19 10:55:06 +0200

    Added more exercises

---
+++ 3.C#-Advanced/9.2.IteratorsAndComparators-Exercise/01.ListyIterator/ListyIterator.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IteratorsAndComparators
+{
+    public class ListyIterator<T>
+    {
+        public List<T> Items { get; set; }
+        public int Index { get; set; }
+        public int Count { get; set; }
+
+        public ListyIterator(List<T> items)
+        {
+            Items = items;
+            Index = 0;
+            Count = items.Count;
+        }
+        public bool Move()
+        {
+            if (Index + 1 < Count)
+            {
+                Index++;
+                return true;
+            }
+            return false;
+        }
+        public bool HasNext()
+        {
+            return Index < Count - 1;
+        }
+        public void Print()
+        {
+            if (Count == 0)
+            {
+                throw new InvalidOperationException("Invalid Operation!");
+            }
+            Console.WriteLine(Items[Index]);
+        }
+    }
+}
