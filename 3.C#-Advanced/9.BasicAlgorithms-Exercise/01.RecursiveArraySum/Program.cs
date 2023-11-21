c98691faeb7789149ae428c2b70d33311edcb067
Anton Antonov <119543923+TripleFlacko@users.noreply.github.com>
2023-11-21 16:30:23 +0200

    Added more exercises

---
+++ 3.C#-Advanced/9.BasicAlgorithms-Exercise/01.RecursiveArraySum/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace _01.RecursiveArraySum
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            var input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            Console.WriteLine(RecursiveSum(input, 0));
+            }
+        private static int RecursiveSum(int[] numbers, int index)
+        {
+            if (index == numbers.Length - 1)
+            {
+                return numbers[index];
+            }
+            return numbers[index] + RecursiveSum(numbers, index + 1);
+        }
+    }
+}
