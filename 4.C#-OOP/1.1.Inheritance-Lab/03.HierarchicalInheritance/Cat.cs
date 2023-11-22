5d804e98960835e45ad3f5d6f0a1d757665782c8
Anton Antonov <anton.anton0v@icloud.com>
2023-11-22 19:43:24 +0200

    Added more exercises

---
+++ 4.C#-OOP/1.1.Inheritance-Lab/03.HierarchicalInheritance/Cat.cs
@@ -0,0 +1,17 @@
+﻿using Farm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Farm
+{
+    public class Cat : Animal
+    {
+        public void Meow()
+        {
+            Console.WriteLine("meowing...");
+        }
+    }
+}
---
+++ 4.C#-OOP/1.1.Inheritance-Lab/03.HierarchicalInheritance/StartUp.cs
@@ -0,0 +1,16 @@
+﻿namespace Farm
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var dog = new Dog();
+            dog.Eat();
+            dog.Bark();
+
+            var cat = new Cat();
+            cat.Eat();
+            cat.Meow();
+        }
+    }
+}
