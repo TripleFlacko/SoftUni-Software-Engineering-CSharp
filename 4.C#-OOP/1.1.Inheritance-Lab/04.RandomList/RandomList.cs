db2c6c15f867c9aa09811127d7bb5f6c4c4bc464
Anton Antonov <anton.anton0v@icloud.com>
2023-11-22 19:52:49 +0200

    Added more exercises

---
+++ 4.C#-OOP/1.1.Inheritance-Lab/04.RandomList/RandomList.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomRandomList
+{
+    public class RandomList : List<string>
+    {
+        public List<string> list { get; set; }
+        public string RandomString()
+        {
+            var random = new Random();
+            int randomIndex = random.Next(list.Count);
+
+            string randomString = list[randomIndex];
+            list.Remove(randomString);
+
+            return randomString;
+        }
+    }
+}
---
+++ 4.C#-OOP/1.1.Inheritance-Lab/05.StackOfStrings/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _05.StackOfStrings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
