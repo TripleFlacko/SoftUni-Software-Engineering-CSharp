62102fabf0a8b423aa43ff143e681ed85ecca838
Anton Antonov <anton.anton0v@icloud.com>
2023-11-22 19:18:37 +0200

    Added more exercises

---
+++ 4.C#-OOP/1.1.Inheritance-Lab/02.MultipleInheritance/Animal.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Farm
+{
+    public class Animal
+    {
+        public void Eat()
+        {
+            Console.WriteLine("eating...");
+        }
+    }
+}
---
+++ 4.C#-OOP/1.1.Inheritance-Lab/02.MultipleInheritance/StartUp.cs
@@ -0,0 +1,13 @@
+﻿namespace Farm
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var puppy = new Puppy();
+            puppy.Eat();
+            puppy.Bark();
+            puppy.Weep();
+        }
+    }
+}
