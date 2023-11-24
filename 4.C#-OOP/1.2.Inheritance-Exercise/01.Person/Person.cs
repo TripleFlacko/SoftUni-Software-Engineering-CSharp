4d2a10182146ad98dcba8529b8db9e3d5232da3e
Anton Antonov <119543923+TripleFlacko@users.noreply.github.com>
2023-11-24 14:45:56 +0200

    Added more exercises

---
+++ 4.C#-OOP/1.2.Inheritance-Exercise/01.Person/Person.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Person
+{
+    public class Person
+    {
+        private string name;
+        private byte age;
+
+        public Person(string name, byte age)
+        {
+            Name = name;
+            Age = age;
+        }
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        public byte Age
+        {
+            get { return age; }
+            set
+            {
+                if (value < 0)
+                {
+                    throw new InvalidOperationException("Age can't be negative!");
+                }
+                age = value;
+            }
+        }
+        public override string ToString()
+        {
+            return $"Name: {Name}, Age: {Age}";
+        }
+    }
+}
