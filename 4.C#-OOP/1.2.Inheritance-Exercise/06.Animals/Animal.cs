138d9f7153c5708e7b07029f5cb4844f8ab81400
Anton Antonov <119543923+TripleFlacko@users.noreply.github.com>
2023-11-24 17:06:25 +0200

    Added more exercises

---
+++ 4.C#-OOP/1.2.Inheritance-Exercise/06.Animals/Animal.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Animals
+{
+    public abstract class Animal
+    {
+        private string name;
+        private int age;
+        private string gender;
+
+        protected Animal(string name, int age, string gender)
+        {
+            Name = name;
+            Age = age;
+            Gender = gender;
+        }
+
+        public string Name
+        {
+            get { return name; }
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("Invalid input!");
+                }
+                name = value;
+            }
+        }
+
+        public int Age
+        {
+            get { return age; }
+            set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException("Invalid input!");
+                }
+                age = value;
+            }
+        }
+
+        public string Gender
+        {
+            get { return gender; }
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("Invalid input!");
+                }
+                gender = value;
+            }
+        }
+        public override string ToString()
+        {
+            return $"{GetType().Name}{Environment.NewLine}{Name} {Age} {Gender}{Environment.NewLine}{ProduceSound()}";
+        }
+        public abstract string ProduceSound();
+    }
+}
---
+++ 4.C#-OOP/1.2.Inheritance-Exercise/06.Animals/StartUp.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Linq;
+
+namespace Animals
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            string input;
+            while ((input = Console.ReadLine()) != "Beast!")
+            {
+                var animalInfo = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).ToArray();
+                try
+                {
+                    switch (input)
+                    {
+                        case "Cat":
+                            var cat = new Cat(animalInfo[0], int.Parse(animalInfo[1]), animalInfo[2]);
+                            Console.WriteLine(cat);
+                            break;
+                        case "Dog":
+                            var dog = new Dog(animalInfo[0], int.Parse(animalInfo[1]), animalInfo[2]);
+                            Console.WriteLine(dog);
+                            break;
+                        case "Frog":
+                            var frog = new Frog(animalInfo[0], int.Parse(animalInfo[1]), animalInfo[2]);
+                            Console.WriteLine(frog);
+                            break;
+                        case "Kitten":
+                            var kitten = new Kitten(animalInfo[0], int.Parse(animalInfo[1]));
+                            Console.WriteLine(kitten);
+                            break;
+                        case "Tomcat":
+                            var tomcat = new Tomcat(animalInfo[0], int.Parse(animalInfo[1]));
+                            Console.WriteLine(tomcat);
+                            break;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+        }
+    }
+}
