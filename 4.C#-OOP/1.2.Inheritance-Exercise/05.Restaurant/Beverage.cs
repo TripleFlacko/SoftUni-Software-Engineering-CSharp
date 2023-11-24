073c66248f2224d1d2b7f4d09b364171ff10b61f
Anton Antonov <119543923+TripleFlacko@users.noreply.github.com>
2023-11-24 15:41:17 +0200

    Added more exercises

---
+++ 4.C#-OOP/1.2.Inheritance-Exercise/05.Restaurant/Beverage.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Restaurant
+{
+    public abstract class Beverage : Product
+    {
+        protected Beverage(string name, decimal price, double milliliters) : base(name, price)
+        {
+        }
+        public double Milliliters { get; set; }
+    }
+}
---
+++ 4.C#-OOP/1.2.Inheritance-Exercise/05.Restaurant/Product.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Restaurant
+{
+    public abstract class Product
+    {
+        protected Product(string name, decimal price)
+        {
+            Name = name;
+            Price = price;
+        }
+
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+    }
+}
