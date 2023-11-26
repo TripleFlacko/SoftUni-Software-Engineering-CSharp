29703867631f04c68d7b756f1195034684d7f44c
Anton Antonov <anton.anton0v@icloud.com>
2023-11-26 12:52:39 +0200

    Added more exercises

---
+++ 4.C#-OOP/1.2.Encapsulation-Lab/04.Team/StartUp.cs
@@ -0,0 +1,18 @@
+﻿namespace PersonsInfo
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var lines = int.Parse(Console.ReadLine());
+            var team = new Team("SoftUni");
+            for (int i = 0; i < lines; i++)
+            {
+                var playerInfo = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).ToArray();
+                var player = new Person(playerInfo[0], playerInfo[1], int.Parse(playerInfo[2]), decimal.Parse(playerInfo[3]));
+                team.AddPlayer(player);
+            }
+            Console.WriteLine(team);
+        }
+    }
+}
---
+++ 4.C#-OOP/1.2.Encapsulation-Lab/04.Team/Team.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonsInfo
+{
+    public class Team
+    {
+		private string name;
+		private List<Person> firstTeam;
+		private List<Person> reserveTeam;
+
+        public Team(string name)
+        {
+            Name = name;
+			firstTeam = new List<Person>();
+			reserveTeam = new List<Person>();
+        }
+
+        public IReadOnlyList<Person> ReserveTeam
+		{
+			get { return reserveTeam; }
+		}
+
+		public IReadOnlyList<Person> FirstTeam
+		{
+			get { return firstTeam; }
+		}
+
+		public string Name
+		{
+			get { return name; }
+			set { name = value; }
+		}
+		public void AddPlayer(Person player)
+		{
+			if (player.Age < 40)
+			{
+				firstTeam.Add(player);
+			}
+			else
+			{
+				reserveTeam.Add(player);
+			}
+		}
+		public override string ToString()
+		{
+			return $"First team has {FirstTeam.Count} players.{Environment.NewLine}Reserve team has {ReserveTeam.Count} players.";
+		}
+	}
+}
