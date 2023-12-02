d06d482142b3ddc6178f625ed08e4a9b58f9a672
Anton Antonov <anton.anton0v@icloud.com>
2023-12-02 14:11:49 +0200

    Added more exercises

--- 4.C#-OOP/4.1.Polymorphism-Lab/02.Animals/Animal.cs
+++ 4.C#-OOP/4.1.Polymorphism-Lab/02.Animals/Animal.cs
@@ -1,11 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Animals
+﻿namespace Animals
 {
     public abstract class Animal
     {
--- 4.C#-OOP/4.1.Polymorphism-Lab/03.Shapes/Shape.cs
+++ 4.C#-OOP/4.1.Polymorphism-Lab/03.Shapes/Shape.cs
@@ -1,16 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Shapes
+﻿namespace Shapes
 {
     public abstract class Shape
     {
         public abstract double CalculatePerimeter();
         public abstract double CalculateArea();
         public virtual string Draw() => "Drawing ";
-  
+
     }
 }
