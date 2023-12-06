4fc4f10610db49553437352bd0c3fae59092d0ee
Anton Antonov <119543923+TripleFlacko@users.noreply.github.com>
2023-12-06 11:16:56 +0200

    Added more exercises

---
+++ 4.C#-OOP/5.ExceptionsAndErrorHandling-Lab/02.EnterNumbers/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace _02.EnterNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var list = new List<int>();
+
+            for (int i = 0; i < 10; i++)
+            {
+                try
+                {
+                    int number = int.Parse(Console.ReadLine());
+
+                    if (number <= 1 || number > 100)
+                    {
+                        throw new ArgumentOutOfRangeException($"Your number is not in range 1 - 100!");
+                    }
+                    list.Add(number);
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Invalid number!");
+                    i--;
+                }
+                catch (ArgumentOutOfRangeException ex)
+                {
+                    Console.WriteLine(ex.ParamName);
+                    i--;
+                }
+            }
+            Console.WriteLine(string.Join(", ", list));
+        }
+    }
+}
