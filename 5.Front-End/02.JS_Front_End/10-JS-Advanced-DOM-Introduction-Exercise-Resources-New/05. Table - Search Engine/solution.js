e4df481c02b9fb63fb2a7772ee46af18b48468df
Anton Antonov <119543923+TripleFlacko@users.noreply.github.com>
2024-08-21 12:34:03 +0300

    Added more exercises

--- 5.Front-End/02.JS_Front_End/10-JS-Advanced-DOM-Introduction-Exercise-Resources-New/05. Table - Search Engine/solution.js
+++ 5.Front-End/02.JS_Front_End/10-JS-Advanced-DOM-Introduction-Exercise-Resources-New/05. Table - Search Engine/solution.js
@@ -2,7 +2,25 @@ function solve() {
    document.querySelector('#searchBtn').addEventListener('click', onClick);
 
    function onClick() {
-      //   TODO:
+      let text = document.getElementById('searchField');
 
+      let rows = document.querySelectorAll('.container tbody tr');
+
+      rows.forEach(row => {
+         row.classList.remove('select');
+      })
+      
+      rows.forEach(row => {
+         const cells = row.children;
+         
+         for (const cell of cells) {
+            const toFind = text.value.toLowerCase();
+            const toCheck = cell.textContent.toLocaleLowerCase();
+
+            if (toCheck.includes(toFind)) {
+               row.classList.add('select');
+            }
+         }   
+      });
    }
 }
