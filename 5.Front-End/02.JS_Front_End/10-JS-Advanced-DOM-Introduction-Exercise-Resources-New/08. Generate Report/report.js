57dd1b66ff1e7274cc4e5da4e6c11af5fae797a1
Anton Antonov <119543923+TripleFlacko@users.noreply.github.com>
2024-08-22 15:52:15 +0300

    Added more exercises

--- 5.Front-End/02.JS_Front_End/10-JS-Advanced-DOM-Introduction-Exercise-Resources-New/08. Generate Report/report.js
+++ 5.Front-End/02.JS_Front_End/10-JS-Advanced-DOM-Introduction-Exercise-Resources-New/08. Generate Report/report.js
@@ -1,3 +1,37 @@
 function generateReport() {
-    //TODO
+    const table = document.querySelector('table');
+    const headers = Array.from(document.querySelectorAll('thead th'));
+    const rows = document.querySelectorAll('tbody tr');
+
+    const output = document.querySelector('#output');
+
+    const result = [];
+
+    const selectedColumns = headers
+    .map((header, index) => {
+        const checkbox = header.querySelector('input[type="checkbox"]')
+
+        if (checkbox && checkbox.checked) {
+            return {
+                name: checkbox.name,
+                index: index
+            }
+        }
+
+        return null;
+    })
+    .filter(column => column !== null);
+
+    rows.forEach(row => {
+        const cells = Array.from(row.querySelectorAll('td'));
+        const rowData = {};
+
+        selectedColumns.forEach(column => {
+            rowData[column.name] = cells[column.index].textContent.trim();
+        })
+
+        result.push(rowData);
+    })
+
+    output.value = JSON.stringify(result, null, 2);
 }
