784273e234cde4b250ed6d4f2316b7ad37c7c0ca
Anton Antonov <119543923+TripleFlacko@users.noreply.github.com>
2024-08-27 17:55:54 +0300

    Added more exercises

--- 5.Front-End/02.JS_Front_End/10-JS-Advanced-DOM-Introduction-Exercise-Resources-New/09. Number Convertor/solution.js
+++ 5.Front-End/02.JS_Front_End/10-JS-Advanced-DOM-Introduction-Exercise-Resources-New/09. Number Convertor/solution.js
@@ -1,4 +1,42 @@
 function solve() {
+    const input = document.querySelector('input');
+    const result = document.querySelector('#result');
 
-    //TODO...
+    const button = document.querySelector('#container button');
+    button.addEventListener('click', onClick);
+
+    const selectMenuTo = document.querySelector('#selectMenuTo');
+    const optionBinary = document.querySelector('#selectMenuTo option');
+    optionBinary.value = 'binary';
+    optionBinary.textContent = 'Binary';
+
+    const optionHexadecimal = document.createElement('option');
+    optionHexadecimal.value = 'hexadecimal';
+    optionHexadecimal.textContent = 'Hexadecimal';
+
+    selectMenuTo.appendChild(optionHexadecimal);
+
+    function onClick() {
+        const convertTo = selectMenuTo.value;
+        
+        switch (convertTo) {
+            case 'binary':
+                result.value = convertToBinary(Number(input.value));
+                break;
+        
+            case 'hexadecimal':
+                result.value = convertToHexadecimal(Number(input.value));
+                break;
+        }
+    }
+
+    function convertToBinary(number) {
+        const binaryNumber = number.toString(2);
+        return binaryNumber;
+    }
+
+    function convertToHexadecimal(number) {
+        const hexadecimalNumber = number.toString(16).toUpperCase();
+        return hexadecimalNumber;
+    }
 }
